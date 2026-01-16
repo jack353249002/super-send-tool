5a79ca3119d061b80c624c89cac00b7b2a4e31d6
jack353249002 <353249002@qq.com>
2026-01-16 12:44:11 +0800

    添加check_user_alive表

--- sql/script.sql
+++ sql/script.sql
@@ -13,3 +13,16 @@ create table super_send_conn_info
     conn_last_login_time   integer default 0 not null
 );
 
+CREATE TABLE check_user_alive (
+     "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+     "username" varchar(50) NOT NULL,
+     "password" text NOT NULL,
+     "day_login_first_time" integer NOT NULL,
+     "send_id" integer,
+     "send_email_action_timeout" INT,
+     "salt" text NOT NULL,
+     "last_send_email_time" integer NOT NULL DEFAULT 0,
+     "super_send_conn_info_id" integer,
+     "position" TEXT,
+     "last_ping_time" integer
+);
